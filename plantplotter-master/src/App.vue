a31bf38ca09c32d6ae83c51377ed289240c9eec4
Jacob Mancini <jmancini021@gmail.com>
2023-05-25 20:14:20 +0930

    test commit

---
+++ plantplotter-master/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <PlantsPage />
+  <br>
+  <DrawPage />
+</template>
+
+<script>
+import DrawPage from './components/DrawPage.vue'
+import PlantsPage from './components/PlantsPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DrawPage,
+    PlantsPage,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ plantplotter-master/src/components/PlantsPage.vue
@@ -0,0 +1,53 @@
+<template>
+    <table>
+        <tr>
+            <td v-for="item in plants" :key="item.plant">
+                <img :src=item.image style="width: 85px; height: 85px;">
+            </td>
+        </tr>
+        <tr>
+            <td v-for="(value, key, index) in plants" :key="index" style="padding-left: 5px; padding-right: 5px;" >
+                <button v-on:click="add(key)">+1 {{ plants[key].plant }}</button> 
+                <!-- Randomly generate plant in garden function needs to be added to onclick -->
+                <button v-on:click="remove(key)">-1</button>
+            </td>
+        </tr>
+        <tr>
+            <td v-for="(value, key, index) in plants" :key="index">
+                <input :id="key" type="text" value = 0 style="width: 40px">
+            </td>
+        </tr>
+    </table>
+</template>
+
+<script>
+import plantsInfo from "../plants.json"
+
+export default {
+    data () {
+        return {
+            plants: plantsInfo,
+            countStuff: []
+        }
+    },
+    mounted () {
+    },
+    methods: {
+        // +1 from plant count
+        add (id) {
+            document.getElementById(id).value = Number(document.getElementById(id).value) + 1;
+        },
+        // -1 from plant count
+        remove (id) {
+            if (document.getElementById(id).value > 0)
+            document.getElementById(id).value = Number(document.getElementById(id).value) -1;
+        }
+    }
+}
+</script>
+
+<style>
+li {
+    display: inline-block;
+}
+</style>
